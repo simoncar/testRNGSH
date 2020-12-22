d58a03ca66fefbf6cf9a9baa7a1f080b8e06d07d
Simoncar <simoncar@gmail.com>
2020-12-22 12:36:14 -1000

    Update count.ts

--- count.ts
+++ count.ts
@@ -2,8 +2,8 @@ import GlobalStore from 'react-native-global-state-hooks';
 
 const countStore = new GlobalStore(0);
 
-const nameStore = new GlobalStore("Initial Name", null, 'NAMEx_STORE');
-const memberStore = new GlobalStore("Initial Color", null, 'MEMBERx_STORE');
+const nameStore = new GlobalStore("Initial Name", null, 'NAME_STORE');
+const memberStore = new GlobalStore("Initial Color", null, 'MEMBER_STORE');
 
 
 export const useCount = countStore.getHook();
